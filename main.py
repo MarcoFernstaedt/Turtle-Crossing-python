214b51ecfb303d11844c53335d4a43b57b175065
Marco <fernstaedtmarco@gmail.com>
2022-10-09 16:39:45 -0700

    built ability to detect when player has won and resets them to sartig point for next round

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
-from scoreboard import Scoreboard
+# from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -22,3 +22,12 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+
+    # Detect collusion with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
